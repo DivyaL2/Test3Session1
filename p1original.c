34f039009ea2bd46d9e99a56ede249010a3de879
DivyaL2 <96220816+DivyaL2@users.noreply.github.com>
2022-03-02 04:59:09 +0000

    done the code

--- p1original.c
+++ p1original.c
@@ -1,2 +1,34 @@
-
+#include<stdio.h>
+void input(int *num1, int *den1, int *num2, int *den2)
+{
+  printf("enter the two fractions\n");
+  scanf("&d&d&d&d",num1,den1,num2,den2);
 }
+void add(int num1,int den1, int num2, int den2,  int*x,int*y,int *gcd)
+{
+    x=((num1 * den2)+(den1 * num2));
+    y= (den1 * den2);
+    for(int i=1;i<=x && i<=y;++i)
+      {
+        if(x%i==0 && y%i==0)
+         gcd =i; 
+      }
+  
+}
+void output(int num1, int den1, int num2, int den2,int x,int y,int gcd)
+{
+  printf("the sum of two fractions is %d/%d",x/gcd,y/gcd);
+  printf("\n");
+}
+int main()
+{
+  int num1,den1,num2,den2,x,y,z;
+  input(&num1,&den1,&num2,&den2);
+   add(num1,den1,num2,den2,&x,&y,&z);
+  output(num1,den1,num2,den2,x,y,z);
+  return 0;
+}
+
+
+
+
